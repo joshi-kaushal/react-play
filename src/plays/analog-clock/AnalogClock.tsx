4e81542c7739139d3a909044b734a5eeffa352e9
Kaushal Joshi <7joshikaushal@gmail.com>
2022-08-03 02:11:49 +0530

    feat: analog clock migrated to TS

--- src/plays/analog-clock/AnalogClock.tsx
+++ src/plays/analog-clock/AnalogClock.tsx
@@ -3,11 +3,9 @@ import "./AnalogClock.css";
 import PlayHeader from "common/playlists/PlayHeader";
 import { useState, useEffect } from "react";
 
-function AnalogClock(props) {
-
-  // Your Code Start below.
-
+function AnalogClock(props: any) {
   const [date, setDate] = useState(new Date());
+
   useEffect(() => {
     const interval = setInterval(() => {
       setDate(new Date());
@@ -15,9 +13,9 @@ function AnalogClock(props) {
 
     return () => clearInterval(interval);
   }, []);
-  const hour = date.getHours() * 30 + 180; // 360/12 = 30 each hour it rotates 30deg +180 (initial design at 0 deg was upside down so to fix it i rotate 180deg..// see the design at 0deg for more clarification)
-  const minute = date.getMinutes() * 6 + 180; // 360/60 = 6 each minutes it rotate 6deg + 180(same as above to ...see at 0 deg)
-  const second = date.getSeconds() * 6 + 180; //// 360/60 = 6 each minutes it rotate 6deg + 180(look at 0 deg for more clarification)
+  const hour: number = date.getHours() * 30 + 180; // 360/12 = 30 each hour it rotates 30deg +180 (initial design at 0 deg was upside down so to fix it i rotate 180deg..// see the design at 0deg for more clarification)
+  const minute: number = date.getMinutes() * 6 + 180; // 360/60 = 6 each minutes it rotate 6deg + 180(same as above to ...see at 0 deg)
+  const second: number = date.getSeconds() * 6 + 180; //// 360/60 = 6 each minutes it rotate 6deg + 180(look at 0 deg for more clarification)
 
   return (
     <>
@@ -49,7 +47,6 @@ function AnalogClock(props) {
               <div className="brand"></div>
             </div>
           </div>
-          {/* Your Code Ends Here */}
         </div>
       </div>
     </>
